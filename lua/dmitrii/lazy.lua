fa051b39ddb52ae17eb959af28fa6025e4f389ee
Dmitrii_Simonov <dmitsimonov@x5.ru>
2025-06-22 17:00:10 +0300

    install lsp

--- lua/dmitrii/lazy.lua
+++ lua/dmitrii/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dmitrii.plugins")
+require("lazy").setup({ { import = "dmitrii.plugins" }, { import = "dmitrii.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
--- lua/dmitrii/plugins/telescope.lua
+++ lua/dmitrii/plugins/telescope.lua
@@ -48,5 +48,6 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set('n', '<leader>fo', ':Telescope lsp_document_symbols<CR>', { desc = '[F]ile [S]tructure' })
   end,
 }
