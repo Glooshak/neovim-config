a0854da1a5203eb34c0333da741089b52ccacb4d
Dmitrii_Simonov <dmitsimonov@x5.ru>
2025-06-23 22:12:36 +0300

    fix setup

--- lua/dmitrii/core/init.lua
+++ lua/dmitrii/core/init.lua
@@ -1,10 +1,2 @@
 require("dmitrii.core.options")
 require("dmitrii.core.keymaps")
-
--- For Python files, explicitly enable spell-check in strings
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
-  callback = function()
-    vim.cmd('syntax spell toplevel')  -- Override syntax restrictions
-  end,
-})
--- lua/dmitrii/plugins/lsp/lspconfig.lua
+++ lua/dmitrii/plugins/lsp/lspconfig.lua
@@ -79,7 +79,14 @@ return {
     end
 
     mason_lspconfig.setup_handlers({
+      -- default handler for installed servers
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
       ["pyright"] = function()
+        -- configure pyright server
         lspconfig.pyright.setup({
           capabilities = capabilities,
           settings = {
